a95b433090153fb038f74c0c22d89495bef651a7
salim <salimtison@gmail.com>
2021-08-18 00:57:03 +0200

    modal overlay

--- src/Component/Crypto/Crypto.tsx
+++ src/Component/Crypto/Crypto.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {CryptoType, Currency} from "../../utils/interfaces";
 import './Crypto.css';
 
@@ -9,6 +9,27 @@ const Crypto = () => {
     const [result1, setResult1] = useState<Array<Currency>>();
     let data: Array<CryptoType>;
     let data1: Array<Currency>;
+
+    const descriptionEl = useRef<HTMLParagraphElement>(null);
+    const closeEL = useRef<HTMLParagraphElement>(null);
+
+    const toggleText = () => {
+        // descriptionEl?.current?.classList.add('overlay');
+        // descriptionEl?.current?.classList.remove('description');
+        descriptionEl?.current?.classList.replace('description', 'overlay');
+        if (closeEL.current)
+            closeEL.current.style.display = 'flex';
+
+
+    }
+
+    const closeModal = () => {
+        // descriptionEl?.current?.classList.add('description');
+        // descriptionEl?.current?.classList.remove('overlay');
+        descriptionEl?.current?.classList.replace('overlay', 'description');
+        if (closeEL.current)
+            closeEL.current.style.display = 'none';
+    }
 
 
     const fetchCryptoIntro = async (api: string) => {
@@ -46,9 +67,17 @@ const Crypto = () => {
             <div className="container">
                 <p className="cryptoName">{result?.[0].name}</p>
                 <img src={result?.[0].logo_url} alt="" className="cryptoLogo"/>
-                <p className="description">{result?.[0].description}</p>
-                <p>Price: {result1?.[0].price}</p>
-                <a href="https://nomics.com" className="link">Crypto Market Cap & Pricing Data Provided By Nomics</a>
+                <div ref={descriptionEl} className="description"><p
+                    className="modal-content description-content">{result?.[0].description}</p>
+                    <p ref={closeEL} className="modal-content close" onClick={closeModal}>X</p>
+                </div>
+
+                <div className="btn-container">
+                    <button onClick={toggleText} className="btn">Voir plus</button>
+                </div>
+                <p className="price">Price: {result1?.[0].price}</p>
+                <a href="https://nomics.com" className="link">Crypto Market Cap & Pricing Data Provided By
+                    Nomics</a>
             </div>
         </div>
     );
